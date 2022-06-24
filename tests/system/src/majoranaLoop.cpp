a61172c24fc163548e9cdeec1f510af3220b7e10
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-06-24 13:36:45 +0200

    Added majorana loop test

--- tests/system/src/majoranaLoop.cpp
+++ tests/system/src/majoranaLoop.cpp
@@ -1,5 +1,5 @@
-#include <marty.h>
 #include "testutility.h"
+#include <marty.h>
 
 using namespace std;
 
@@ -10,13 +10,10 @@ csl::Expr cc(csl::Expr const &init)
 
 void Check(std::vector<csl::Expr> const &ampl)
 {
-    for (const auto& a : ampl)
-        if (csl::AnyOfLeafs(a, [&](csl::Expr const &sub)
-            {
-                return csl::IsITensor(sub)
-                    and mty::dirac4.isGammaTensor(sub);
-            })) 
-        {
+    for (const auto &a : ampl)
+        if (csl::AnyOfLeafs(a, [&](csl::Expr const &sub) {
+                return csl::IsITensor(sub) and mty::dirac4.isGammaTensor(sub);
+            })) {
             if (csl::IsSum(a))
                 mty::Display(a->getVectorArgument());
             else
@@ -25,10 +22,8 @@ void Check(std::vector<csl::Expr> const &ampl)
         }
 }
 
-int main() {
-
-    std::cout.setstate(std::ios_base::failbit);
-    std::cerr.setstate(std::ios_base::failbit);
+int main()
+{
 
     mty::option::decomposeInOperators = false;
     mty::Model model;
@@ -36,11 +31,11 @@ int main() {
     model.init();
     model.renameParticle("A_U_1", "\\gamma");
 
-    int Q = 1; // U(1) charge of S, P and psi
+    int           Q      = 1; // U(1) charge of S, P and psi
     mty::Particle psi    = diracfermion_s("\\psi", model);
     mty::Particle lambda = diracfermion_s("\\lambda", model);
-    mty::Particle S = scalarboson_s("S", model);
-    mty::Particle P = scalarboson_s("P", model);
+    mty::Particle S      = scalarboson_s("S", model);
+    mty::Particle P      = scalarboson_s("P", model);
 
     csl::Expr m = csl::constant_s("m");
     psi->setGroupRep("U_1", Q);
@@ -53,27 +48,23 @@ int main() {
 
     model.addParticles({psi, lambda, S, P});
 
-    csl::Tensor delta = mty::dirac4.getDelta();
-    csl::Tensor gamma5 = mty::dirac4.gamma_chir;
-    csl::Tensor gamma = mty::dirac4.gamma;
-    mty::Particle A = model.getParticle("\\gamma");
-    csl::Index a1 = mty::DiracIndex();
-    csl::Index a2 = mty::DiracIndex();
-    csl::Index mu = mty::MinkowskiIndex();
-    csl::Expr SP_term = CSL_HALF * Q * Q * csl::pow_s(cc(S)*P - cc(P)*S, 2);
-    csl::Expr psiL_term = 
-        CSL_I * Q * cc(psi(a1)) * 
-        (
-            S * delta({a1, a2}) 
-            + CSL_I * gamma5({a1, a2}) * P
-        ) 
-        * lambda(a2);
+    csl::Tensor   delta  = mty::dirac4.getDelta();
+    csl::Tensor   gamma5 = mty::dirac4.gamma_chir;
+    csl::Tensor   gamma  = mty::dirac4.gamma;
+    mty::Particle A      = model.getParticle("\\gamma");
+    csl::Index    a1     = mty::DiracIndex();
+    csl::Index    a2     = mty::DiracIndex();
+    csl::Index    mu     = mty::MinkowskiIndex();
+    csl::Expr     SP_term
+        = CSL_HALF * Q * Q * csl::pow_s(cc(S) * P - cc(P) * S, 2);
+    csl::Expr psiL_term
+        = CSL_I * Q * cc(psi(a1))
+          * (S * delta({a1, a2}) + CSL_I * gamma5({a1, a2}) * P) * lambda(a2);
     model.addLagrangianTerm(SP_term);
     model.addLagrangianTerm(psiL_term, true);
 
-    model.addLagrangianTerm(
-            CSL_HALF*csl::GetComplexConjugate(lambda(a1))*A(mu)*gamma({+mu,a1,a2})*lambda(a2)
-            );
+    model.addLagrangianTerm(CSL_HALF * csl::GetComplexConjugate(lambda(a1))
+                            * A(mu) * gamma({+mu, a1, a2}) * lambda(a2));
 
     std::cout << model << std::endl;
 
@@ -81,11 +72,11 @@ int main() {
 
     Display(feynmanRules);
 
-    auto majoranaLoop = model.computeAmplitude(
-            mty::Order::OneLoop,
-            {Incoming(S), Incoming(AntiPart(S)),
-            Outgoing(S), Outgoing(AntiPart(S))}
-            );
+    auto majoranaLoop = model.computeAmplitude(mty::Order::OneLoop,
+                                               {Incoming(S),
+                                                Incoming(AntiPart(S)),
+                                                Outgoing(S),
+                                                Outgoing(AntiPart(S))});
 
     Display(majoranaLoop);
 
@@ -95,24 +86,26 @@ int main() {
         auto OK = [&](csl::Expr const &expr) {
             return csl::AllOfLeafs(expr, [&](csl::Expr const &sub) {
                 return !csl::IsIndicialTensor(sub)
-                    || bool(dynamic_cast<mty::QuantumField const*>(sub.get()));
+                       || bool(
+                           dynamic_cast<mty::QuantumField const *>(sub.get()));
             });
         };
         if (!OK(diagExpr)) {
             error(buildMessage("Diagram could not be simplified",
-                        " with Majorana fermion loops: \n", diagExpr));
+                               " with Majorana fermion loops: \n",
+                               diagExpr));
             return 1;
         }
     }
 
-    majoranaLoop = model.computeAmplitude(
-            mty::Order::OneLoop,
-            {Incoming(P), Incoming(AntiPart(P)),
-            Outgoing(P), Outgoing(AntiPart(P))}
-            );
+    majoranaLoop = model.computeAmplitude(mty::Order::OneLoop,
+                                          {Incoming(P),
+                                           Incoming(AntiPart(P)),
+                                           Outgoing(P),
+                                           Outgoing(AntiPart(P))});
 
     Display(majoranaLoop);
-    //Show(majoranaLoop);
+    // Show(majoranaLoop);
 
     for (const auto &diagram : majoranaLoop.getDiagrams()) {
         csl::Expr diagExpr = diagram.getExpression();
@@ -120,12 +113,14 @@ int main() {
         auto OK = [&](csl::Expr const &expr) {
             return csl::AllOfLeafs(expr, [&](csl::Expr const &sub) {
                 return !csl::IsIndicialTensor(sub)
-                    || bool(dynamic_cast<mty::QuantumField const*>(sub.get()));
+                       || bool(
+                           dynamic_cast<mty::QuantumField const *>(sub.get()));
             });
         };
         if (!OK(diagExpr)) {
             error(buildMessage("Diagram could not be simplified",
-                        " with Majorana fermion loops: \n", diagExpr));
+                               " with Majorana fermion loops: \n",
+                               diagExpr));
             return 1;
         }
     }
