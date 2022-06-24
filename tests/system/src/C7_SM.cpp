632d7213d9362504e63a4bae73332416ded1418c
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-06-24 10:58:17 +0200

    First test integrated

--- tests/system/src/C7_SM.cpp
+++ tests/system/src/C7_SM.cpp
@@ -1,15 +1,11 @@
-#include <marty.h>
+#include "comparedata.h"
 #include "marty/models/sm.h"
+#include <marty.h>
 
 using namespace csl;
 using namespace mty;
 using namespace std;
 using namespace sm_input;
-
-#define RESET       "\033[0m"
-#define MESSAGEFONT "\033[1m\033[1;34m"
-
-// #define PRINT_PROG
 
 int calculate(Model &model, gauge::Type gauge)
 {
@@ -20,62 +16,44 @@ int calculate(Model &model, gauge::Type gauge)
     model.getParticle("W")->setGaugeChoice(gauge);
 
     mty::FeynOptions options;
-    options.addFilters(
-            mty::filter::forceParticle("t")
-            );
+    options.addFilters(mty::filter::forceParticle("t"));
     auto res = model.computeAmplitude(
-                OneLoop,
-                {Incoming("b"), 
-                Outgoing("s"), 
-                Outgoing("A")},
-                options);
+        OneLoop, {Incoming("b"), Outgoing("s"), Outgoing("A")}, options);
     Display(res);
 
-    Expr V_ts_star = csl::GetComplexConjugate(sm_input::V_ts);
-    Expr s2_theta_W = csl::pow_s(csl::sin_s(theta_W), 2);
+    Expr V_ts_star      = csl::GetComplexConjugate(sm_input::V_ts);
+    Expr s2_theta_W     = csl::pow_s(csl::sin_s(theta_W), 2);
     Expr factorOperator = -csl::pow_s(e_em, 3) * V_ts_star * V_tb * m_b
-        / (32 * CSL_PI*CSL_PI * M_W*M_W * s2_theta_W);
+                          / (32 * CSL_PI * CSL_PI * M_W * M_W * s2_theta_W);
 
     options.setWilsonOperatorCoefficient(factorOperator);
     auto wilsonC7 = model.getWilsonCoefficients(res, options);
     Display(wilsonC7);
-    Expr CC7   = getWilsonCoefficient(
-            wilsonC7,
-            chromoMagneticOperator(model, wilsonC7, DiracCoupling::R));
-    Expr CC7p  = getWilsonCoefficient(
-            wilsonC7,
-            chromoMagneticOperator(model, wilsonC7, DiracCoupling::L));
+    Expr CC7 = getWilsonCoefficient(
+        wilsonC7, chromoMagneticOperator(model, wilsonC7, DiracCoupling::R));
+    Expr CC7p = getWilsonCoefficient(
+        wilsonC7, chromoMagneticOperator(model, wilsonC7, DiracCoupling::L));
     std::cout << CC7 << std::endl;
     std::cout << CC7p << '\n';
 
-    [[maybe_unused]] int sysres = system("rm -r C7_SM");
-    mty::Library wilsonLib("C7_SM", ".");
+    [[maybe_unused]] int sysres = system("rm -rf libs/C7_SM");
+    mty::Library         wilsonLib("C7_SM", "libs");
     wilsonLib.cleanExistingSources();
     wilsonLib.addFunction("C7", CC7);
     wilsonLib.addFunction("C7_p", CC7p);
     wilsonLib.print();
 
-    sysres = system("cp libscripts/example_c7_sm.cpp C7_SM/script");
-    sysres = system("cd C7_SM; make >/dev/null && bin/example_c7_sm.x > /dev/null");
+    sysres = system("cp libsrc/example_c7_sm.cpp libs/C7_SM/script");
+    sysres = system("cd libs/C7_SM; make && bin/example_c7_sm.x");
     std::cout.clear();
-    sysres = system(
-            ("bin/comparedata.x "
-            "data/C7_SM.txt "
-            "C7_SM/C7_SM.txt "
-            + toString(((gauge == gauge::Unitary) ? "1e-3" : "1e-5"))
-            ).c_str());
-#ifndef PRINT_PROG
-    std::cout.setstate(std::ios_base::failbit);
-#endif
-    return sysres;
+    int assertion = assert_equal("data/output/C7_SM.txt",
+                                 "libs/C7_SM/C7_SM.txt",
+                                 (gauge == gauge::Unitary) ? 1e-3 : 1e-5);
+    return assertion;
 }
 
-int main() {
-
-#ifndef PRINT_PROG
-    std::cout.setstate(std::ios_base::failbit);
-    std::cerr.setstate(std::ios_base::failbit);
-#endif
+int main()
+{
 
     SM_Model sm;
     std::cout << sm << std::endl;
--- tests/system/src/comparedata.h
+++ tests/system/src/comparedata.h
@@ -1,18 +1,16 @@
-#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
 
-using namespace std;
+#define RESET "\033[0m"
+#define ERRORFONT "\033[1m\033[31m"   /* Red */
+#define SUCCESSFONT "\033[1m\033[32m" /* Green */
 
-#define RESET       "\033[0m"
-#define ERRORFONT   "\033[1m\033[31m"      /* Red */
-#define SUCCESSFONT "\033[1m\033[32m"      /* Green */
-
-std::vector<double> readLine(std::ifstream &fin)
+inline std::vector<double> readLine(std::ifstream &fin)
 {
     std::vector<double> res;
     res.reserve(5);
@@ -28,7 +26,7 @@ std::vector<double> readLine(std::ifstream &fin)
     return res;
 }
 
-double compare(double a, double b, double)
+inline double compare(double a, double b, double)
 {
     const auto diff = std::abs(a - b) / ((a < 1e-10) ? 1 : a);
     // if (diff > eps) {
@@ -39,17 +37,15 @@ double compare(double a, double b, double)
     return diff;
 }
 
-std::pair<double, double> compare(
-        std::vector<double> const &A,
-        std::vector<double> const &B,
-        double                     eps)
+inline std::pair<double, double>
+compare(std::vector<double> const &A, std::vector<double> const &B, double eps)
 {
     if (A.size() != B.size()) {
         return {-1, -1};
     }
-    double diff = 0;
-    auto iterA = A.begin();
-    auto iterB = B.begin();
+    double diff    = 0;
+    auto   iterA   = A.begin();
+    auto   iterB   = B.begin();
     double maxdiff = 0;
     while (iterA != A.end()) {
         auto diff_i = compare(*iterA, *iterB, eps);
@@ -63,73 +59,66 @@ std::pair<double, double> compare(
     return {(A.size() == 0) ? 0 : diff / A.size(), maxdiff};
 }
 
-std::pair<double, double> compare(
-        char const *file1,
-        char const *file2,
-        double      eps
-        )
+inline std::pair<double, double>
+compare(char const *file1, char const *file2, double eps)
 {
     std::ifstream f1(file1);
     if (!f1) {
-        std::cerr << ERRORFONT << "Error:" << RESET << " file \"" 
-            << file1 << "\" not found!\n";
+        std::cerr << ERRORFONT << "Error:" << RESET << " file \"" << file1
+                  << "\" not found!\n";
         return {-1, -1};
     }
     std::ifstream f2(file2);
     if (!f2) {
-        std::cerr << ERRORFONT << "Error:" << RESET << " file \""
-            << file2 << "\" not found!\n";
+        std::cerr << ERRORFONT << "Error:" << RESET << " file \"" << file2
+                  << "\" not found!\n";
         return {-1, -1};
     }
     std::pair<double, double> diff = {0, 0};
-    size_t n = 0;
+    size_t                    n    = 0;
     while (!f1.eof()) {
         auto diff_i = compare(readLine(f1), readLine(f2), eps);
         if (diff_i.first == -1 || diff_i.second == -1) {
-            std::cout << ERRORFONT << "Error: size mismatch in results" << RESET 
-                << '\n';
+            std::cout << ERRORFONT << "Error: size mismatch in results"
+                      << RESET << '\n';
             return {-1, -1};
         }
         diff.first += diff_i.first;
-        if (diff_i.second > diff.second) 
+        if (diff_i.second > diff.second)
             diff.second = diff_i.second;
         ++n;
     }
     if (!f2.eof()) {
         std::cerr << ERRORFONT << "Error:" << RESET << " file mismatch for \""
-            << file1 << "\" and " << "\"" << file2 << "\" !\n";
+                  << file1 << "\" and "
+                  << "\"" << file2 << "\" !\n";
         return {-1, -1};
     }
 
-    if (n != 0) return {diff.first / n, diff.second};
+    if (n != 0)
+        return {diff.first / n, diff.second};
     return diff;
 }
 
-int main(int argc, char **argv) {
-
-    if (argc < 4) {
-        std::cerr << ERRORFONT << "Error: This program needs 3 arguments: ";
-        std::cerr << "the names of the two data files to compare and the ";
-        std::cerr << "precision to impose.\n" << RESET;
-        exit(1);
-    }
+inline int
+assert_equal(std::string const &fileA, std::string const &fileB, double eps)
+{
     srand(time(0));
     std::cout.precision(15);
-    char *file1 = *(argv + 1);
-    char *file2 = *(argv + 2);
-    double eps = std::atof(*(argv + 3));
 
-    auto res = compare(file1, file2, eps);
+    auto res = compare(fileA.c_str(), fileB.c_str(), eps);
     if (res.first >= 0 && res.second >= 0 && res.second < eps) {
-        std::cout << SUCCESSFONT << "Successful comparison of file \"" 
-            << file1 << "\" and file \"" << file2 << "\": \n" << RESET;
+        std::cout << SUCCESSFONT << "Successful comparison of file \"" << fileA
+                  << "\" and file \"" << fileB << "\": \n"
+                  << RESET;
         std::cout << "Average relative error = " << res.first << std::endl;
         std::cout << "Maximum relative error = " << res.second << std::endl;
         return 0;
     }
     else {
-        std::cout << ERRORFONT << "Discrepancy between file \"" 
-            << file1 << "\" and file \"" << file2 << "\": \n" << RESET;
+        std::cout << ERRORFONT << "Discrepancy between file \"" << fileA
+                  << "\" and file \"" << fileB << "\": \n"
+                  << RESET;
         std::cout << "Average relative error = " << res.first << std::endl;
         std::cout << "Maximum relative error = " << res.second << std::endl;
         return 1;
