4078093c3e89d7a60660b2ddd76984b5ac8da666
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-06-24 14:28:29 +0200

    Prepared test full MSSM

--- tests/system/src/test_fullMSSM.cpp
+++ tests/system/src/test_fullMSSM.cpp
@@ -1,32 +1,30 @@
-#include <marty.h>
+#include "comparedata.h"
 #include "marty/models/pmssm_lem.h"
 #include <fstream>
+#include <marty.h>
 #include <sstream>
 
 using namespace mty;
 
-std::vector<std::vector<mty::Insertion>> read_processes(
-        std::istream &file)
+std::vector<std::vector<mty::Insertion>> read_processes(std::istream &file)
 {
     std::vector<std::vector<mty::Insertion>> res;
     res.reserve(3000);
     std::string line;
     std::getline(file, line);
-    while (!line.empty())
-    {
+    while (!line.empty()) {
         std::string token;
         res.push_back(std::vector<mty::Insertion>{});
         auto &process = res.back();
         process.reserve(4);
         std::istringstream line_stream(line);
-        for (int i = 0; i != 4; ++i)
-        {
+        for (int i = 0; i != 4; ++i) {
             line_stream >> token;
-            bool conj = false;
-            const size_t len = token.size();
-            if (len > 2 && token[len-2] == '#') {
-                token = std::string(token.begin(), token.begin() + len-2);
-                conj = true;
+            bool         conj = false;
+            const size_t len  = token.size();
+            if (len > 2 && token[len - 2] == '#') {
+                token = std::string(token.begin(), token.begin() + len - 2);
+                conj  = true;
             }
             auto insertion = (i < 2) ? mty::Incoming : mty::Outgoing;
             if (conj) {
@@ -41,58 +39,50 @@ std::vector<std::vector<mty::Insertion>> read_processes(
     return res;
 }
 
-
 std::string getProcessName(std::vector<mty::Insertion> const &process)
 {
     std::string name;
-    for (size_t i = 0; i != process.size(); ++i)
-    {
+    for (size_t i = 0; i != process.size(); ++i) {
         name += process[i].getField()->getName();
-        if (!process[i].isParticle())
-        {
+        if (!process[i].isParticle()) {
             name += "_c";
         }
-        if (i == 1)
-        {
+        if (i == 1) {
             name += "_to_";
         }
     }
     return name;
 }
 
-
 void calculateProcesses(
-        mty::Model &model,
-        mty::Library &lib,
-        std::vector<std::vector<mty::Insertion>> const &processes,
-        bool unitary
-        )
+    mty::Model                                     &model,
+    mty::Library                                   &lib,
+    std::vector<std::vector<mty::Insertion>> const &processes,
+    bool                                            unitary)
 {
-    if (unitary)
-    {
+    if (unitary) {
         model.getParticle("W")->setGaugeChoice(mty::gauge::Unitary);
         model.getParticle("Z")->setGaugeChoice(mty::gauge::Unitary);
     }
-    else
-    {
+    else {
         model.getParticle("W")->setGaugeChoice(mty::gauge::Feynman);
         model.getParticle("Z")->setGaugeChoice(mty::gauge::Feynman);
     }
-    for (const auto &process : processes)
-    {
-        auto ampl = model.computeAmplitude(TreeLevel, process);
-        auto squared_ampl = model.computeSquaredAmplitude(ampl);
-        std::string processName = getProcessName(process);
+    for (const auto &process : processes) {
+        auto        ampl         = model.computeAmplitude(TreeLevel, process);
+        auto        squared_ampl = model.computeSquaredAmplitude(ampl);
+        std::string processName  = getProcessName(process);
         lib.addFunction(processName, squared_ampl);
     }
 }
 
-int main() {
+int main()
+{
 
-    PMSSM_LEM pmssm;
-    std::ifstream process_file("processes.marty");
-    auto feynman_processes = read_processes(process_file);
-    auto unitary_processes = read_processes(process_file);
+    PMSSM_LEM     pmssm;
+    std::ifstream process_file("data/input/processes.marty");
+    auto          feynman_processes = read_processes(process_file);
+    auto          unitary_processes = read_processes(process_file);
     process_file.close();
 
     mty::Library lib("full_MSSM");
@@ -103,7 +93,6 @@ int main() {
     calculateProcesses(pmssm, lib, unitary_processes, true);
 
     lib.print();
-
 
     return 0;
 }
