e9e1d8cb7959013a681d898a5231fc2696803453
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-06-24 13:08:15 +0200

    C8_PMSSM and gm2 tests

--- tests/system/libsrc/example_gm2_sm.cpp
+++ tests/system/libsrc/example_gm2_sm.cpp
@@ -1,47 +1,53 @@
-#include "gm2_sm.h"
-#include <fstream>
 #include "clooptools.h"
+#include "gm2sm.h"
 #include "marty/core/looptools_init.h"
 #include "marty/core/looptools_interface.h"
+#include <fstream>
 
-using namespace gm2_sm;
+using namespace gm2sm;
 
 // PDG
 double gm2_theo(param_t const &param)
 {
-  auto GF = param.alpha_em*4*M_PI *std::sqrt(2)/(8*powq(param.M_W*sinq(param.theta_W), 2));
-  return GF * (double)(param.m_e*param.m_e) / (8*std::sqrt(2)*M_PI*M_PI)
-    * (5./3 + 1./3 * powq(1 - 4*powq(sinq(param.theta_W), 2), 2));
+    auto GF = param.alpha_em * 4 * M_PI * std::sqrt(2)
+              / (8 * powq(param.M_W * sinq(param.theta_W), 2));
+    return GF * (double) (param.m_e * param.m_e)
+           / (8 * std::sqrt(2) * M_PI * M_PI)
+           * (5. / 3 + 1. / 3 * powq(1 - 4 * powq(sinq(param.theta_W), 2), 2));
 }
 
-int main() {
+int main()
+{
 
-  // std::cout.precision(10);
-  // std::cout << mty::lt::C0iC(18, 1, 1.0001, 0, 1.56e6, 1, 1, mty::lt::reg_int) << '\n';
-  //return 0;
-  param_t param;
-  param.alpha_em = 1./137;
-  param.M_W = 80.379;
-  param.M_Z = 91.2;
-  param.m_Gp = 80.379;
-  param.m_G0 = 91.2;
-  param.theta_W = acosq(param.M_W / param.M_Z);
-  std::ofstream real("real.txt");
-  std::ofstream imag("imag.txt");
+    // std::cout.precision(10);
+    // std::cout << mty::lt::C0iC(18, 1, 1.0001, 0, 1.56e6, 1, 1,
+    // mty::lt::reg_int) << '\n';
+    // return 0;
+    param_t param;
+    param.alpha_em = 1. / 137;
+    param.M_W      = 80.379;
+    param.M_Z      = 91.2;
+    param.m_Gp     = 80.379;
+    param.m_G0     = 91.2;
+    param.theta_W  = acosq(param.M_W / param.M_Z);
+    std::ofstream real("real.txt");
+    std::ofstream imag("imag.txt");
 
-  getIntegrationParameters()->reg_int = 1e-8;
-  int n1(0), n2(0);
-  // Numerical instability partly regularized below 10^(-4)
+    getIntegrationParameters()->reg_int = 1e-8;
+    int n1(0), n2(0);
+    // Numerical instability partly regularized below 10^(-4)
     for (double exp_me = -4; exp_me <= +10; exp_me += 0.1) {
-        if (n1 == 1) ++n2;
+        if (n1 == 1)
+            ++n2;
         param.m_e = std::pow(10, exp_me);
         setmudim(param.m_e);
-        param.s_12 = param.m_e*param.m_e;
-        real << (double)param.m_e << '\t' << crealq(gm2_e(param)) << '\t' 
-             << crealq(gm2_eW(param)) << '\t' << param.alpha_em/(2*M_PI) << '\t' << gm2_theo(param) << '\n';
+        param.s_12 = param.m_e * param.m_e;
+        real << (double) param.m_e << '\t' << crealq(gm2_e(param)) << '\t'
+             << crealq(gm2_eW(param)) << '\t' << param.alpha_em / (2 * M_PI)
+             << '\t' << gm2_theo(param) << '\n';
     }
-  real.close();
-  imag.close();
+    real.close();
+    imag.close();
 
     return 0;
 }
--- tests/system/src/C8_PMSSM.cpp
+++ tests/system/src/C8_PMSSM.cpp
@@ -1,43 +1,24 @@
-#include <marty.h>
+#include "comparedata.h"
 #include "marty/models/pmssm_lem.h"
+#include <marty.h>
 
 using namespace std;
 using namespace csl;
 using namespace mty;
 
-#define RESET       "\033[0m"
-#define MESSAGEFONT "\033[1m\033[1;34m"
-
-
-// #define PRINT_PROG
-#define MARTY_VERSION 13
-
 int calculate(Model &model)
 {
-    Expr V_ts_star = csl::GetComplexConjugate(sm_input::V_ts);
-    Expr V_tb = sm_input::V_tb;
-    Expr mb = sm_input::m_b;
-    Expr ms = sm_input::m_s;
-    Expr M_W = sm_input::M_W;
-#if MARTY_VERSION >= 13
-    Expr e_em = sm_input::e_em;
-#else
-    Expr e_em = sm_input::e;
-#endif
-    Expr g_s = sm_input::g_s;
-    Expr s2_theta_W = sm_input::s2_theta_W;
+    Expr V_ts_star      = csl::GetComplexConjugate(sm_input::V_ts);
+    Expr V_tb           = sm_input::V_tb;
+    Expr mb             = sm_input::m_b;
+    Expr ms             = sm_input::m_s;
+    Expr M_W            = sm_input::M_W;
+    Expr e_em           = sm_input::e_em;
+    Expr g_s            = sm_input::g_s;
+    Expr s2_theta_W     = sm_input::s2_theta_W;
     Expr factorOperator = -csl::pow_s(e_em, 2) * g_s * V_ts_star * V_tb * mb
-        / (32 * CSL_PI*CSL_PI * M_W*M_W * s2_theta_W);
+                          / (32 * CSL_PI * CSL_PI * M_W * M_W * s2_theta_W);
 
-#ifndef PRINT_PROG
-    std::cout.setstate(std::ios_base::failbit);
-    std::cerr.setstate(std::ios_base::failbit);
-#endif
-
-#if MARTY_VERSION < 13
-    model.diracFermionEmbedding("s_L", "s_R");
-    model.diracFermionEmbedding("b_L", "b_R");
-#endif
     Display(model.getFeynmanRules());
 
     mty::sm_input::M_W->setValue(CSL_UNDEF);
@@ -45,41 +26,18 @@ int calculate(Model &model)
     mty::sm_input::m_s->setValue(CSL_UNDEF);
     mty::sm_input::m_t->setValue(CSL_UNDEF);
 
-    mty::option::decomposeInOperators = false;
-    mty::option::displayAbbreviations = false;
-#if MARTY_VERSION >= 13
+    mty::option::decomposeInOperators    = false;
+    mty::option::displayAbbreviations    = false;
     mty::option::keepOnlyFirstMassInLoop = false;
     FeynOptions options;
     options.setWilsonOperatorCoefficient(factorOperator);
-    options.addFilters(
-            mty::filter::forceParticles({"C_1", "C_2"}),
-            mty::filter::forceParticles({"st_1", "st_2"}),
-            mty::filter::disableParticles({"W", "Gp", "Hp"})
-            );
-    auto res = model.computeAmplitude(
-                Order::OneLoop,
-                {Incoming("b"), 
-                Outgoing("s"), 
-                Outgoing("G")},
-                options);
-#else
-    mty::DisableParticle("W");
-    mty::DisableParticle("Gp");
-    mty::DisableParticle("Hp");
-    mty::DisableParticle("c_L");
-    mty::DisableParticle("c_R");
-    mty::DisableParticle("u_R");
-    mty::DisableParticle("u_L");
-    mty::DisableParticle("sc_L");
-    mty::DisableParticle("sc_R");
-    mty::DisableParticle("su_R");
-    mty::DisableParticle("su_L");
-    auto res = model.computeAmplitude(
-                Order::OneLoop,
-                {Incoming("b"), 
-                Outgoing("s"), 
-                Outgoing("G")});
-#endif
+    options.addFilters(mty::filter::forceParticles({"C_1", "C_2"}),
+                       mty::filter::forceParticles({"st_1", "st_2"}),
+                       mty::filter::disableParticles({"W", "Gp", "Hp"}));
+    auto res
+        = model.computeAmplitude(Order::OneLoop,
+                                 {Incoming("b"), Outgoing("s"), Outgoing("G")},
+                                 options);
 
     DisplayAbbreviations();
     Display(res);
@@ -89,45 +47,36 @@ int calculate(Model &model)
     std::cout << V_tb << '\n';
     std::cout << mb << '\n';
     std::cout << "FACTOR = " << factorOperator << '\n';
-    
+
     auto wilsonC8 = model.getWilsonCoefficients(res);
     Display(wilsonC8);
 
-    // for (const auto &w : wilsonC8) {
-    //     std::cout << csl::Evaluated(w.coef.getCoefficient(), csl::eval::abbreviation) << '\n';
-    // }
-
-    Expr CC8   = getWilsonCoefficient(
-            wilsonC8,
-            chromoMagneticOperator(model, wilsonC8, DiracCoupling::R));
-    Expr CC8p  = getWilsonCoefficient(
-            wilsonC8,
-            chromoMagneticOperator(model, wilsonC8, DiracCoupling::L));
+    Expr CC8 = getWilsonCoefficient(
+        wilsonC8, chromoMagneticOperator(model, wilsonC8, DiracCoupling::R));
+    Expr CC8p = getWilsonCoefficient(
+        wilsonC8, chromoMagneticOperator(model, wilsonC8, DiracCoupling::L));
     std::cout << CC8 << std::endl;
     std::cout << CC8p << std::endl;
-    Display(csl::DeepExpanded(csl::Evaluated(CC8, csl::eval::abbreviation))->getVectorArgument());
-    // std::cin.get();
+    Display(csl::DeepExpanded(csl::Evaluated(CC8, csl::eval::abbreviation))
+                ->getVectorArgument());
 
-    [[maybe_unused]] int sysres = system("rm -r C8_PMSSM");
+    [[maybe_unused]] int sysres = system("rm -rf libs/C8_PMSSM");
     mty::Library::setQuadruplePrecision(true);
-    mty::Library wilsonLib("C8_PMSSM", ".");
+    mty::Library wilsonLib("C8_PMSSM", "libs");
     wilsonLib.addFunction("C8", CC8);
     wilsonLib.addFunction("C8_p", CC8p);
     wilsonLib.generateSpectrum(model);
     wilsonLib.print();
 
-    sysres = system("cp libscripts/example_c8_pmssm.cpp C8_PMSSM/script");
-    sysres = system("cd C8_PMSSM; make >/dev/null&& bin/example_c8_pmssm.x > /dev/null");
-    std::cout.clear();
-    return system(
-            "bin/comparedata.x "
-            "data/C8_marty_tanb50_ss.txt "
-            "C8_PMSSM/C8_marty_tanb50_ss.txt "
-            "1e-5"
-            );
+    sysres = system("cp libsrc/example_c8_pmssm.cpp libs/C8_PMSSM/script");
+    sysres = system("cd libs/C8_PMSSM; make && bin/example_c8_pmssm.x");
+    return assert_equal("data/output/C8_marty_tanb50_ss.txt",
+                        "libs/C8_PMSSM/C8_marty_tanb50_ss.txt",
+                        1e-5);
 }
 
-int main() {
+int main()
+{
 
     auto pmssm_model = mty::PMSSM_LEM();
 
