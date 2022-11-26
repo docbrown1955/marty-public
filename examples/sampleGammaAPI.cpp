2aaea7553c0faaa8b664a5e4225549126d432239
GregoireUhlrich <greg.um@free.fr>
2022-11-26 16:31:02 +0100

    More API use !

--- examples/sampleGammaAPI.cpp
+++ examples/sampleGammaAPI.cpp
@@ -1,19 +1,33 @@
 #include <marty.h>
 #include <marty/api/gamma.h>
 
-namespace gapi        = mty::gamma_api;
-constexpr auto g      = mty::gamma_api::g;
-constexpr auto eps    = mty::gamma_api::eps;
-constexpr auto G      = mty::gamma_api::gamma;
-constexpr auto G5     = mty::gamma_api::gamma5;
-constexpr auto PL     = mty::gamma_api::P_L;
-constexpr auto PR     = mty::gamma_api::P_R;
-constexpr auto fchain = mty::gamma_api::chain;
+namespace gapi                      = mty::gamma_api;
+constexpr auto g                    = mty::gamma_api::g;
+constexpr auto eps                  = mty::gamma_api::eps;
+constexpr gapi::Expr (*G)(int)      = mty::gamma_api::gamma;
+constexpr gapi::Expr (*S)(int, int) = mty::gamma_api::gamma;
+constexpr auto G5                   = mty::gamma_api::gamma5;
+constexpr auto PL                   = mty::gamma_api::P_L;
+constexpr auto PR                   = mty::gamma_api::P_R;
+constexpr auto fchain               = mty::gamma_api::chain;
+
+void logLatexEquation(gapi::Expr const &lhs, gapi::Expr const &rhs)
+{
+    static std::ofstream fout("equations.tex");
+    fout << "\n";
+    fout << "\\begin{equation}\n";
+    fout << "\t\\begin{split}\n";
+    fout << "\t\t" << gapi::generateLatex(lhs) << " = "
+         << gapi::generateLatex(rhs) << '\n';
+    fout << "\t\\end{split}\n";
+    fout << "\\end{equation}\n";
+}
 
 void testChainSimplification(gapi::Expr const &chain)
 {
     std::cout << "Initial chain:    " << chain << std::endl;
     std::cout << "Simplified chain: " << gapi::simplified(chain) << std::endl;
+    logLatexEquation(chain, gapi::simplified(chain));
 }
 
 void testFierzSimplification(gapi::Expr const &chain1,
@@ -21,9 +35,22 @@ void testFierzSimplification(gapi::Expr const &chain1,
 {
     std::cout << "Initial chains:   " << chain1 << " X " << chain2
               << std::endl;
-    std::cout << "Simplified chain: "
-              << gapi::simplified(gapi::applyDoubleFierz(chain1, chain2))
+    gapi::Expr res = gapi::simplified(gapi::applySingleFierz(chain1, chain2));
+    std::cout << "Simplified chain: " << res << std::endl;
+    std::cout << sgl::sgl_to_csl(
+        res, sgl::buildTensorSet(&csl::Minkowski, &mty::dirac4))
               << std::endl;
+    logLatexEquation(chain1 * chain2, res);
+}
+
+void testFierzSimplification2(gapi::Expr const &chain1,
+                              gapi::Expr const &chain2)
+{
+    std::cout << "Initial chains:   " << chain1 << " X " << chain2
+              << std::endl;
+    gapi::Expr res = gapi::simplified(gapi::applyDoubleFierz(chain1, chain2));
+    std::cout << "Simplified chain: " << res << std::endl;
+    logLatexEquation(chain1 * chain2, res);
 }
 
 int main()
@@ -40,14 +67,22 @@ int main()
     testChainSimplification(fchain({G(1), G(2)}, 0, 0));
     testChainSimplification(fchain({G(1), G(2), G(3), G(4), G5()}, 0, 0));
 
-    testFierzSimplification(fchain({G(1), PL()}, 0, 1),
-                            fchain({G(1), PR()}, 2, 3));
-    testFierzSimplification(fchain({G(1), PL()}, 0, 1),
-                            fchain({G(1), PL()}, 2, 3));
-    testFierzSimplification(fchain({G(1), G(2), G(3), PL()}, 0, 1),
-                            fchain({G(1), G(2), G(3), PL()}, 2, 3));
-    testFierzSimplification(fchain({G(1), G(2), G(3), PL()}, 0, 1),
-                            fchain({G(1), G(2), G(3), PR()}, 2, 3));
+    testFierzSimplification2(fchain({G(1), PL()}, 0, 1),
+                             fchain({G(1), PR()}, 2, 3));
+    testFierzSimplification2(fchain({G(1), PL()}, 0, 1),
+                             fchain({G(1), PL()}, 2, 3));
+    testFierzSimplification2(fchain({G(1), G(2), G(3), PL()}, 0, 1),
+                             fchain({G(1), G(2), G(3), PL()}, 2, 3));
+    testFierzSimplification2(fchain({G(1), G(2), G(3), PL()}, 0, 1),
+                             fchain({G(1), G(2), G(3), PR()}, 2, 3));
+    testFierzSimplification(fchain({PL()}, 0, 1), fchain({PR()}, 2, 3));
+    testFierzSimplification(fchain({PL()}, 0, 1), fchain({PL()}, 2, 3));
+
+    testChainSimplification(fchain({S(0, 1), S(2, 3), PL()}, 0, 0));
+    testFierzSimplification(fchain({S(0, 1)}, 0, 1),
+                            fchain({S(0, 1), PL()}, 2, 3));
+    testFierzSimplification(fchain({S(0, 1), PL()}, 0, 1),
+                            fchain({S(0, 1), PL()}, 2, 3));
 
     return 0;
 }
