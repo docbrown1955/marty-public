20e3472e51579fe24b4c3bf6a7258f9d73223e42
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2022-12-11 12:28:24 +0100

    Added permissive covariant csl::Index

--- src/csl/options.h
+++ src/csl/options.h
@@ -34,6 +34,7 @@ inline bool applySelfContractions  = true;
 inline bool applyChainContractions = false;
 
 inline bool fullComparison = true;
+inline bool permissiveCovariantIndices = false;
 inline bool printIndexIds  = true;
 
 } // namespace csl::option
