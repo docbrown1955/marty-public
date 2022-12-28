3f9b26c319903a138444a568b10a00a58d42c27f
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2022-12-28 11:11:26 +0100

    Use copy in sglinterface functions

--- src/marty/sgl/sglinterface.cpp
+++ src/marty/sgl/sglinterface.cpp
@@ -20,19 +20,19 @@ namespace sgl {
 GExpr DeepCopy(GExpr const &expr)
 {
     GExpr res = expr->copy();
-    for (size_t i = 0; i != expr->size(); ++i) {
-        expr->argument(i) = sgl::DeepCopy(expr->argument(i));
+    for (size_t i = 0; i != res->size(); ++i) {
+        res->argument(i) = sgl::DeepCopy(res->argument(i));
     }
-    return expr->copy();
+    return res->copy();
 }
 
 GExpr DeepRefreshed(GExpr const &expr)
 {
-    GExpr res = expr->refresh();
-    for (size_t i = 0; i != expr->size(); ++i) {
-        expr->argument(i) = sgl::DeepRefreshed(expr->argument(i));
+    GExpr res = expr->copy();
+    for (size_t i = 0; i != res->size(); ++i) {
+        res->argument(i) = sgl::DeepRefreshed(res->argument(i));
     }
-    return expr->refresh();
+    return res->refresh();
 }
 
 } // namespace sgl
