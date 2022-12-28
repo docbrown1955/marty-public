f4ef31f601e98511b601a198f90a141b2b2cdf95
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2022-12-28 12:57:14 +0100

    First gamma API pythont tests

---
+++ python/tests/test_import.py
@@ -0,0 +1,16 @@
+def test_import_basics():
+    from pymarty.gamma_api import Expr, FierzBasis, set_symbolic_dimension
+
+
+def test_import_objects():
+    from pymarty.gamma_api import g, eps, gamma, P_L, P_R, gamma5, C, D
+
+
+def test_import_functions():
+    from pymarty.gamma_api import (
+        current,
+        trace,
+        single_fierz,
+        double_fierz,
+        print_latex,
+    )
