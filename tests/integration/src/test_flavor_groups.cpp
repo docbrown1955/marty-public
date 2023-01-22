fdf42ec2b662a487b2b242af08c9854063d61088
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2023-01-22 21:16:54 +0100

    Flavor integration test

---
+++ tests/integration/src/test_flavor_groups.cpp
@@ -0,0 +1,55 @@
+#include "marty.h"
+#include <gtest/gtest.h>
+
+TEST(flavor_groups, test_symbolic_flavor)
+{
+    csl::Expr Nf = csl::constant_s("Nf");
+    mty::Model model;
+    model.addFlavorGroup("F", Nf);
+    model.addGaugedGroup(mty::group::Type::U1, "X");
+    model.init();
+
+    mty::Particle p1 = mty::scalarboson_s("p1", model);
+    p1->setFundamentalFlavorRep("F");
+    p1->setGroupRep("X", 1);
+    mty::Particle p2 = mty::scalarboson_s("p2", model);
+    p2->setFundamentalFlavorRep("F");
+    p2->setGroupRep("X", 1);
+
+    model.addParticles({p1, p2});
+
+    std::cout << model << std::endl;
+
+    csl::Space const *flavorSpace = model.getVectorSpace("F");
+    ASSERT_NE(flavorSpace, nullptr);
+    ASSERT_EQ(flavorSpace->getName(), "F");
+    csl::Index i = model.generateIndex("F");
+    ASSERT_EQ(i.getSpace(), flavorSpace);
+
+    {
+        mty::Amplitude ampl = model.computeAmplitude(
+            mty::Order::TreeLevel,
+            {mty::Incoming("p1"), mty::Incoming(mty::AntiPart("p1")),
+            mty::Outgoing("p2"), mty::Outgoing(mty::AntiPart("p2"))}
+        );
+        csl::Expr squared_ampl = model.computeSquaredAmplitude(ampl);
+        csl::Expr eval = csl::Evaluated(squared_ampl, csl::eval::abbreviation);
+        csl::Expr simplified = csl::DeepHardFactored(csl::DeepExpanded(eval));
+
+        ASSERT_TRUE(simplified->dependsExplicitlyOn(Nf.get()));
+        ASSERT_FALSE((simplified/(Nf*Nf))->dependsExplicitlyOn(Nf.get()));
+    }
+    {
+        mty::Amplitude ampl = model.computeAmplitude(
+            mty::Order::OneLoop,
+            {mty::Incoming("A_X"), mty::Outgoing("A_X")}
+        );
+        csl::Expr squared_ampl = model.computeSquaredAmplitude(ampl);
+        csl::Expr eval = csl::Evaluated(squared_ampl, csl::eval::abbreviation);
+        csl::Expr simplified = csl::DeepHardFactored(csl::DeepExpanded(eval));
+
+        std::cout << simplified << std::endl;
+        ASSERT_TRUE(simplified->dependsExplicitlyOn(Nf.get()));
+        ASSERT_FALSE((simplified/(Nf*Nf))->dependsExplicitlyOn(Nf.get()));
+    }
+}
