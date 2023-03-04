96bd54392b915a4bf9d8d006388f772f7b69fb4a
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2023-03-04 14:47:26 +0100

    Remove conftest.py file

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,5 +1,5 @@
 import sys
 
-pymarty_path = "/mnt/d/marty/marty-public/python/src"
+pymarty_path = "/mnt/d/perfectenshlag/marty/marty-public/python/src"
 if pymarty_path not in sys.path:
     sys.path.append(pymarty_path)
