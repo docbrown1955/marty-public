69295c7bb169f7387aa86f8beabb53302d40eb5b
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2023-07-22 17:51:46 +0200

    Refresh some model examples

--- examples/sampleQED.cpp
+++ examples/sampleQED.cpp
@@ -5,42 +5,33 @@
  * and compute several amplitudes in this model.
  */
 #include <marty.h>
+#include <marty/models/qed.h>
 
 using namespace csl;
 using namespace mty;
 
 int main()
 {
-    Model QED;
-    AddGaugedGroup(QED, group::Type::U1, "em", constant_s("e"));
-    Init(QED);
+    QED_Model qed;
+    std::cout << qed << std::endl;
 
-    Particle electron = diracfermion_s("e", QED);
-    SetMass(electron, "m_e");
-    SetGroupRep(electron, "em", -1); // Set the charge -1 for group em
-    AddParticle(QED, electron);
-    Rename(QED, "A_em", "A"); // By default bosons are names A_<group_name>
-    Particle photon = GetParticle(QED, "A");
-
-    std::cout << QED << std::endl;
-
-    auto rules = ComputeFeynmanRules(QED);
+    auto rules = ComputeFeynmanRules(qed);
     Display(rules);
     Show(rules);
 
-    auto compton = QED.computeAmplitude(Order::TreeLevel,
-                                        {Incoming(electron),
-                                         Incoming(photon),
-                                         Outgoing(electron),
-                                         Outgoing(photon)});
+    auto compton = qed.computeAmplitude(Order::TreeLevel,
+                                        {Incoming("e"),
+                                         Incoming("A"),
+                                         Outgoing("e"),
+                                         Outgoing("A")});
     Display(compton);
     Show(compton);
 
-    auto diff = QED.computeAmplitude(Order::TreeLevel,
-                                     {Incoming(electron),
-                                      Incoming(electron),
-                                      Outgoing(electron),
-                                      Outgoing(electron)});
+    auto diff = qed.computeAmplitude(Order::TreeLevel,
+                                     {Incoming("e"),
+                                      Incoming("e"),
+                                      Outgoing("e"),
+                                      Outgoing("e")});
     Display(diff);
     Show(diff);
     DisplayAbbreviations();
