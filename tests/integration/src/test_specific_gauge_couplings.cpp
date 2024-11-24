7f9549f761fb56718442cedadb0cb35469cc0dc7
gregoire uhlrich <gregoire.uhlrich@siemens.com>
2024-11-24 22:48:38 +0100

    Fix tests for gauge coupling, add multi-particle test

--- tests/integration/src/test_specific_gauge_couplings.cpp
+++ tests/integration/src/test_specific_gauge_couplings.cpp
@@ -31,10 +31,8 @@ TEST(specific_gauge_couplings, scalar)
                 factor, [&l](const csl::Expr &el) { return el == l; });
             ASSERT_TRUE(specific_coupling_found);
 
-            bool normal_coupling_found
-                = csl::FirstOfNode(factor, [&e](const csl::Expr &el) {
-                      return el == csl::pow_s(e, 2);
-                  });
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &el) { return el == e; });
             ASSERT_FALSE(normal_coupling_found);
 
             break;
@@ -117,16 +115,103 @@ TEST(specific_gauge_coupling, fermion)
                 factor, [&l](const csl::Expr &el) { return el == l; });
             ASSERT_TRUE(specific_coupling_found);
 
-            bool normal_coupling_found
-                = csl::FirstOfNode(factor, [&e](const csl::Expr &el) {
-                      return el == csl::pow_s(e, 2);
-                  });
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &el) { return el == e; });
             ASSERT_FALSE(normal_coupling_found);
 
             break;
         }
     }
     ASSERT_TRUE(term_found);
+}
+
+TEST(specific_gauge_coupling, fermion_multi)
+{
+    csl::Expr  e = csl::constant_s("e");
+    mty::Model QED;
+    QED.addGaugedGroup(mty::group::Type::U1, "em", e);
+    QED.init();
+
+    QED.renameParticle("A_em", "A");
+    mty::Particle A = QED.getParticle("A");
+
+    mty::Particle el = mty::diracfermion_s("e", QED);
+    el->setGroupRep("em", -1); // Charge -1
+    csl::Expr C_e
+        = csl::constant_s("C_e"); // Define coupling constant for electron
+    el->setSpecificGaugeCoupling("em", C_e);
+
+    mty::Particle mu = mty::diracfermion_s("mu", QED);
+    mu->setGroupRep("em", -1); // Charge -1
+    csl::Expr C_mu
+        = csl::constant_s("C_mu"); // Define coupling constant for electron
+    mu->setSpecificGaugeCoupling("em", C_mu);
+
+    mty::Particle tau = mty::diracfermion_s("tau", QED);
+    tau->setGroupRep("em", -1); // Charge -1
+
+    QED.addParticles({el, mu, tau});
+
+    std::cout << QED << std::endl;
+
+    // Electron part
+    bool term_found = false;
+    for (const auto &term : QED.getLagrangian().interaction) {
+        if (term->count(el.get()) == 2 && term->count(A.get()) == 1) {
+            term_found       = true;
+            csl::Expr factor = term->getGlobalFactor();
+
+            bool specific_coupling_found = csl::FirstOfLeaf(
+                factor, [&C_e](const csl::Expr &ex) { return ex == C_e; });
+            ASSERT_TRUE(specific_coupling_found);
+
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &ex) { return ex == e; });
+            ASSERT_FALSE(normal_coupling_found);
+
+            break;
+        }
+    }
+    ASSERT_TRUE(term_found);
+
+    // Muon part
+    term_found = false;
+    for (const auto &term : QED.getLagrangian().interaction) {
+        if (term->count(mu.get()) == 2 && term->count(A.get()) == 1) {
+            term_found       = true;
+            csl::Expr factor = term->getGlobalFactor();
+
+            bool specific_coupling_found = csl::FirstOfLeaf(
+                factor, [&C_mu](const csl::Expr &ex) { return ex == C_mu; });
+            ASSERT_TRUE(specific_coupling_found);
+
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &ex) { return ex == e; });
+            ASSERT_FALSE(normal_coupling_found);
+
+            break;
+        }
+    }
+    ASSERT_TRUE(term_found);
+
+    // Tau part
+    term_found = false;
+    for (const auto &term : QED.getLagrangian().interaction) {
+        if (term->count(tau.get()) == 2 && term->count(A.get()) == 1) {
+            term_found       = true;
+            csl::Expr factor = term->getGlobalFactor();
+
+            bool specific_coupling_found = csl::FirstOfLeaf(
+                factor, [&C_mu](const csl::Expr &ex) { return ex == C_mu; });
+            ASSERT_FALSE(specific_coupling_found);
+
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &ex) { return ex == e; });
+            ASSERT_TRUE(normal_coupling_found);
+
+            break;
+        }
+    }
 }
 
 TEST(specific_gauge_coupling, vector)
@@ -161,10 +246,8 @@ TEST(specific_gauge_coupling, vector)
                 factor, [&l](const csl::Expr &el) { return el == l; });
             ASSERT_TRUE(specific_coupling_found);
 
-            bool normal_coupling_found
-                = csl::FirstOfNode(factor, [&e](const csl::Expr &el) {
-                      return el == csl::pow_s(e, 2);
-                  });
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &el) { return el == e; });
             ASSERT_FALSE(normal_coupling_found);
 
             break;
@@ -203,10 +286,8 @@ TEST(specific_gauge_coupling, vector_quadratic_term)
                 factor, [&l](const csl::Expr &el) { return el == l; });
             ASSERT_TRUE(specific_coupling_found);
 
-            bool normal_coupling_found
-                = csl::FirstOfNode(factor, [&e](const csl::Expr &el) {
-                      return el == csl::pow_s(e, 2);
-                  });
+            bool normal_coupling_found = csl::FirstOfLeaf(
+                factor, [&e](const csl::Expr &el) { return el == e; });
             ASSERT_FALSE(normal_coupling_found);
 
             break;
